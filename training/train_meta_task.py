ea733bd98977f1d81f32b07a2fd90bb8dcd96a36
Nan Jiang <jiang@gpu3.hcics.simtech.uni-stuttgart.de>
2024-11-14 17:08:39 +0100

    NAN

--- training/train_meta_task.py
+++ training/train_meta_task.py
@@ -87,7 +87,7 @@ class TrainState(TrainState):
 class TrainConfig:
     project: str = "xminigrid"
     group: str = "default"
-    name: str = "local_cnn_with_ssp"
+    name: str = "cnn_with_ssp"
     env_id: str = "XLand-MiniGrid-R1-9x9"
     benchmark_id: str = "trivial-1m"
     img_obs: bool = False 
@@ -104,12 +104,12 @@ class TrainConfig:
     lstm_hidden_dim: int = 512
     # training
     enable_bf16: bool = False
-    num_envs: int = 2048
-    num_steps_per_env: int = 4096
+    num_envs: int = 4096
+    num_steps_per_env: int = 2048
     num_steps_per_update: int = 32
     update_epochs: int = 1
     num_minibatches: int = 16
-    total_timesteps: int = 1_000_000_000
+    total_timesteps: int = 1_000_000_00
     lr: float = 0.001
     clip_eps: float = 0.2
     gamma: float = 0.99
